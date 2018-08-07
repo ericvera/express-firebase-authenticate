7a5861568908368e7c45ca39e552a1da26320c93
Eric Vera <hey@itsmeeric.com>
2018-08-07 12:58:17 -0300

    Add index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+const bearerToken = require('express-bearer-token')
+const firebaseAdmin = require('firebase-admin')
+
+module.exports = async (req, res, next) => {
+  bearerToken(req, res, async () => {
+    try {
+      req.user = await firebaseAdmin.auth().verifyIdToken(req.token)
+
+      return next()
+    } catch (error) {}
+
+    res.sendStatus(401)
+  })
+}
